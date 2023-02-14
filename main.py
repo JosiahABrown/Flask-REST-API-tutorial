b26a6958e4dcf72960b9feb42b0ab72be371cdfd
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-02-14 14:55:52 -0600

    Request Argument Parser

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"Josiah": {"age": 20, "gender": "male"},
-         "Tim": {"age": 22, "gender": "male"}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the Video")
+video_put_args.add_argument("views", type=int, help="Views of the Video")
+video_put_args.add_argument("likes", type=int, help="Likes of the Video")
+
+videos = {}
 
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 
-    def post(self):
-        return {"data": "Posted"}
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
