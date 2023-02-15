35607a4e9b1a5b127c2d9aa32a28c9fe70014150
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-02-15 14:44:56 -0600

    Handling delete requests

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the Video", required=True)
+video_put_args.add_argument("name", type=str, help="Name of the Video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the Video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the Video", required=True)
 
 videos = {}
 
 
+def abort_if_video_id_not_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id is not valid...")
+
+
+def abort_if_video_id_exist(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID...")
+
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_not_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_id_exist(video_id)
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_not_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,5 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+data = [{"likes": 77, "name": "Josiah", "views": 100000},
+        {"likes": 100000, "name": "How to make REST API", "views": 800000},
+        {"likes": 30, "name": "Tim does a back flip", "views": 20000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
